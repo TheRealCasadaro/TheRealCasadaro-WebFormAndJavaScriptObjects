c09232ff8f7e75e61ca952e3b87264c0a01729c0
TheRealCasadaro <casadaro@casadaro.com>
2020-11-02 17:53:58 -0800

    created class object to hold values user input values. Renameed VerifyUserUpdateFuction to CollectUserInformation to more acurately describe what he fuction does. added code to the CollectUserInformation Fuction that gets the user information from the main HTML page and stores it in an object for further processing.

--- main.ts
+++ main.ts
@@ -9,6 +9,19 @@
     located in the root directory.
 */
 
+
+// this class is used to create an object 
+// that can will hold user inputs
+class NewUserDomain
+{
+    UserName:string;
+    EmailAddress:string;
+    PhoneNumber:string;
+    //The Phone number must be a valid Phone number
+    DesireDomainName:string;
+    //desiredWebsiteName can only contain characters lowercase characters a to z,numbers, and dashes(-) to separate words. 
+}
+
 // The window is what the user see's when that app loads.
 // This function get the button from the document and adds a click even lisenter.
 window.onload = function()
@@ -16,11 +29,38 @@ window.onload = function()
     // place the button object in memory by assigning it to a variable
     let SumbitUserInformationButton = <HTMLElement>document.querySelector("input[type=button]");
     // gets the button in memory and adds a function that us executed when the button is clicked
-    SumbitUserInformationButton.onclick = VerifyUserInfo; //VerifyUserInfo is a function that Verifies User Info
+    SumbitUserInformationButton.onclick = CollectUserInformation; //VerifyUserInfo is a function that Verifies User Info
 }
 
-//This fuction will verify the users input(s).
-function VerifyUserInfo () {
-    // TASKS
-    alert("Button Clicked");
+
+
+//This fuction gathers the users input from the the main HTML page and returns an object that contains everything that was entered in the form.
+function CollectUserInformation ():NewUserDomain {
+    // create a new object named userInput to hold the users input in memory.
+    let userInput = new NewUserDomain();
+    
+    /* There are other ways to do the follow using loops, recursion, and or some 
+    other methodes that would obviously be more complicated. 
+    For now, for personal sanity and readabilty, each input is manually retrieved.
+    */
+
+    // To keep the code easily readable, at least thats the logic for now.
+    // gets the user input field from the main html document.
+    let userName = <HTMLInputElement>document.getElementById("UserName")
+    let userEmailAddress = <HTMLInputElement>document.getElementById("UserEmailAddress")
+    let userTelephoneNumber = <HTMLInputElement>document.getElementById("UserTelephoneNumber")
+    let userDesiredDomainName = <HTMLInputElement>document.getElementById("UserTelephoneNumber")
+    
+    
+    //Now we will places the value contained in the input field in the userInput Object in memeory.
+    userInput.UserName = userName.value;
+    userInput.EmailAddress = userEmailAddress.value;
+    userInput.PhoneNumber = userTelephoneNumber.value;
+    userInput.DesireDomainName = userDesiredDomainName.value;
+
+    return userInput;
+}
+
+function DisplayUserInput(){
+    // TODO: Display users  verified input below the input form.
 }
